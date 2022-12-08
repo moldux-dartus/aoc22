6a0c2b84f0b47a58052db501b0ecd7c9816d29f5
user <user@pop-os.localdomain>
2022-12-08 17:50:45 -0600

    Day 8 complete imbecile style

--- Day8.hs
+++ Day8.hs
@@ -4,50 +4,52 @@ import Data.List
 
 day8 = do
   t <- lines <$> readFile "inputs/input8.txt"
-  print $ part1 (grid t) -- (map (take 15) (take 15 t))
-  return ()
-
-part1 :: Grid -> Int
-part1 grid = length
-                $ filter (\tree -> visibleFromUp grid tree
-                                || visibleFromLeft grid tree
-                                || visibleFromRight grid tree
-                                || visibleFromDown grid tree) grid
+  print $ part1 (grid t)
+  print $ part2 (grid t)
 
 type Table = [String]
 type Coords = (Int, Int)
 type Grid = [Tree]
 type Tree = (Coords, Int)
 
+up (x,y) = (x,y-1)
+down (x,y) = (x,y+1)
+left (x,y) = (x-1,y)
+right (x,y) = (x+1,y)
+
 grid :: Table -> Grid
 grid table = zip (coords table) (map digitToInt $ concat table)
   where coords side = [ (y,x) | x <- [1..(length side)], y <- [1..(length side)] ]
 
+part1 :: Grid -> Int
+part1 grid = length
+           $ filter (\tree -> visibleFrom up grid tree
+                           || visibleFrom left grid tree
+                           || visibleFrom right grid tree
+                           || visibleFrom down grid tree) grid
 
-visibleFromLeft :: Grid -> Tree -> Bool
-visibleFromLeft grid ((x,y), t) = Just t > maximum (queryLeft (x-1,y))
-  where queryLeft (x,y) = let lk = lookup (x,y) grid
-                          in lk : if (lk > Just t) || isNothing (lookup (x-1,y) grid)
-                                  then [Nothing]
-                                  else queryLeft (x-1,y)
+part2 :: Grid -> Int
+part2 grid = maximum $
+             map (\t -> distanceFrom left grid t
+                      * distanceFrom right grid t
+                      * distanceFrom up grid t
+                      * distanceFrom down grid t) grid
 
-visibleFromRight :: Grid -> Tree -> Bool
-visibleFromRight grid ((x,y), t) = Just t > maximum (queryRight (x+1,y))
-  where queryRight (x,y) = let lk = lookup (x,y) grid
-                           in lk : if isNothing (lookup (x+1,y) grid)
-                                   then [Nothing]
-                                   else queryRight (x+1,y)
+distanceFrom :: (Coords -> Coords) -> Grid -> Tree -> Int
+distanceFrom dir grid ((x,y), t)
+  | isNothing lk = 0
+  | Just t <= lk = 1
+  | otherwise = 1 + distanceFrom dir grid (dir (x,y), t)
+  where lk = lookup (dir (x,y)) grid
 
-visibleFromUp :: Grid -> Tree -> Bool
-visibleFromUp grid ((x,y), t) = Just t > maximum (queryUp (x,y-1))
-  where queryUp (x,y) = let lk = lookup (x,y) grid
-                        in lk : if isNothing (lookup (x,y-1) grid)
-                                then [Nothing]
-                                else queryUp (x,y-1)
+visibleFrom :: (Coords -> Coords) -> Grid -> Tree -> Bool
+visibleFrom dir grid ((x,y), t) = Just t > maximum (query $ dir (x,y))
+  where query (x,y) = let lk = lookup (x,y) grid
+                      in  lk : if isNothing (lookup (dir (x,y)) grid) then [Nothing]
+                               else query $ dir (x,y)
 
-visibleFromDown :: Grid -> Tree -> Bool
-visibleFromDown grid ((x,y), t) = Just t > maximum (queryUp (x,y+1))
-  where queryUp (x,y) = let lk = lookup (x,y) grid
-                        in lk : if isNothing (lookup (x,y+1) grid)
-                                then [Nothing]
-                                else queryUp (x,y+1)
+t = [ 30373
+    , 25512
+    , 65332
+    , 33549
+    , 35390]
